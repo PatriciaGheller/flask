690d4850a94f2f78c2caf5e5f0e70be006b75312
Patrícia Gheller <patricia.gheller.1985@gmail.com>
2026-01-25 12:29:13 -0300

    Conexão com banco de dados - part2

--- dio_bank/src/app.py
+++ dio_bank/src/app.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask
 
 
@@ -8,7 +6,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'diobank.sqlite'),
+        DATABASE='diobank.sqlite',
     )
 
     if test_config is None:
@@ -17,11 +15,9 @@ def create_app(test_config=None):
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
+  
+    from . import db
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    db.init_app(app)
 
     return app
